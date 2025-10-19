5eed9ca425ceed0b15e33070c7541bd0a215d0d5
Psyborx <psyborx@gmail.com>
2025-10-18 21:41:34 -0700

    Fix tabs config and unnecessary function calls

--- GmailTabs.user.js
+++ GmailTabs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gmail Tabs
 // @namespace    http://psyborx.com/
-// @version      1.17
+// @version      1.18
 // @description  Add custom tabs to Gmail
 // @author       Psyborx
 // @match        *://mail.google.com/mail/u/0/*
@@ -187,7 +187,7 @@
       icon: `${matIconsUrl}sms_white_20dp.png`
     }, {
       id: 'attachment',
-      query: 'attach_or_drive=true',
+      query: '{has:attachment has:drive}',
       label: 'Attachment',
       icon: `${matIconsUrl}attachment_white_20dp.png`
     }, {
@@ -214,173 +214,174 @@
       document.querySelectorAll('.S4').forEach(el => { el.style.height = `calc(100vh - 64px - 16px - ${customTabsContainerHeight})`; });
     },
     init: () => {
-      gmtt.totalWait += gmtt.intWait;
-      gmtt.baseUrl = document.location.origin + document.location.pathname;
-      // console.log('*************** document.location: ', document.location);
-      let display = true;
-      let query = document.location.hash.replace('#', '');
-      const hash = query.split('/');
-      // console.log('*************** Current hash: ', hash);
+      if (document.location.href.includes('#')) {
+        gmtt.totalWait += gmtt.intWait;
+        gmtt.baseUrl = document.location.origin + document.location.pathname;
+        let display = true;
+        let query = document.location.hash.replace('#', '');
+        const hash = query.split('/');
+        // console.log('*************** Current hash: ', hash);
 
-      switch (hash[0]) {
-        case 'settings':
-          display = false;
-          break;
-        case 'inbox':
-        case 'starred':
-        case 'snoozed':
-        case 'sent':
-        case 'scheduled':
-        case 'all':
-        case 'spam':
-        case 'trash':
-          display = gmtt.shouldDisplay(hash, 1);
-          break;
-        case 'label':
-        case 'category':
-        case 'chats':
-          display = gmtt.shouldDisplay(hash, 2);
-          query = hash.slice(0, 2).join('/');
-          break;
-        case 'search':
-        case 'advanced-search': {
-          display = gmtt.shouldDisplay(hash, 2);
-          const isrefinementRegexp = new RegExp(`(${isrefinementParam})$`);
-          query = decodeURIComponent(hash[1]).replace(isrefinementRegexp, '').replace(/[\u002B]/g, ' ');
-          break;
-        }
-      }
-
-      if (display) {
-        const tabCfg = gmtt.tabsConfig.find(tCfg => tCfg.query == query);
-        if (tabCfg) {
-          gmtt.selected = tabCfg.id;
-        } else {
-          gmtt.selected = null;
-        }
-      }
-
-      let tabParent = false;
-      if (hash[0] == 'inbox') {
-        tabParent = document.querySelector('div.bGI.nH.oy8Mbf.aE3.S4 > div.aKh');
-        if (tabParent) {
-          tabParent.style.height = 'fit-content';
-          tabParent.querySelector('.aKk').style.height = 'auto';
-        }
-      } else {
-        tabParent = document.querySelector('.AO');
-      }
-      // console.log('*************** Current Tab Parent: ', tabParent);
-      if (tabParent && document.querySelector('.vX.UC').offsetParent === null) {
-        // console.log('*************** Tab Parent found');
-        gmtt.totalWait = 0;
-        if (!display) {
-          if (gmtt.customTabsContainer && !gmtt.customTabsContainer.classList.contains('displayNone')) {
-            gmtt.customTabsContainer.classList.add('displayNone');
+        switch (hash[0]) {
+          case 'settings':
+            display = false;
+            break;
+          case 'inbox':
+          case 'starred':
+          case 'snoozed':
+          case 'sent':
+          case 'scheduled':
+          case 'all':
+          case 'spam':
+          case 'trash':
+          case 'drafts':
+          case 'chats':
+            display = gmtt.shouldDisplay(hash, 1);
+            break;
+          case 'label':
+          case 'category':
+            display = gmtt.shouldDisplay(hash, 2);
+            query = hash.slice(0, 2).join('/');
+            break;
+          case 'search':
+          case 'advanced-search': {
+            display = gmtt.shouldDisplay(hash, 2);
+            const isrefinementRegexp = new RegExp(`(${isrefinementParam})$`);
+            query = decodeURIComponent(hash[1]).replace(isrefinementRegexp, '').replace(/[\u002B]/g, ' ');
+            break;
           }
-        } else {
-          if (gmtt.customTabsContainer) {
-            if (gmtt.customTabsContainer.classList.contains('displayNone')) {
-              gmtt.customTabsContainer.classList.remove('displayNone');
-            }
-            gmtt.customTabsContainer.querySelectorAll('.customTab').forEach(customTab => {
-              if (gmtt.selected && customTab.id == `ct-${gmtt.selected}`) {
-                if (!customTab.classList.contains('ctSelected')) {
-                  customTab.classList.add('ctSelected');
-                }
-              } else {
-                if (customTab.classList.contains('ctSelected')) {
-                  customTab.classList.remove('ctSelected');
-                }
-              }
-            });
+        }
+
+        if (display) {
+          const tabCfg = gmtt.tabsConfig.find(tCfg => tCfg.query == query);
+          if (tabCfg) {
+            gmtt.selected = tabCfg.id;
           } else {
-            gmtt.customTabBar = document.createElement('div');
-            gmtt.customTabBar.id = 'customTabBar';
-            gmtt.customTabBar.className = 'aKh aKx customTabs customTabsC';
-            const fragment = document.createDocumentFragment();
-            gmtt.tabsConfig.forEach(tabCfg => {
-              const tab = document.createElement('div');
-              tab.id = `ct-${tabCfg.id}`;
-              tab.className = `aKz customTab ${tabCfg.id == gmtt.selected ? ' ctSelected' : ''}`;
-
-              let url = gmtt.baseUrl;
-              if (/^[\w/]+$/.test(tabCfg.query)) {
-                url += `#${tabCfg.query}`;
-              } else if (/(&|=)/.test(tabCfg.query)) {
-                url += `#advanced-search/${encodeURIComponent(tabCfg.query)}${isrefinementParam}`;
-              } else {
-                url += `#search/${encodeURIComponent(tabCfg.query)}`;
-              }
-              const anchor = document.createElement('a');
-              anchor.id = `ct-a-${tabCfg.id}`;
-              anchor.href = url;
-              anchor.setAttribute('target', '_top');
-              anchor.setAttribute('aria-label', tabCfg.label);
-              anchor.setAttribute('tabindex', -1);
-              anchor.setAttribute('draggable', false);
-
-              const aIcon = document.createElement('div');
-              aIcon.className = 'customTabIcon';
-              aIcon.style.backgroundImage = `url('${tabCfg.icon}')`;
-              anchor.append(aIcon);
-
-              const aLabel = document.createElement('div');
-              aLabel.innerText = tabCfg.label;
-              anchor.append(aLabel);
-
-              tab.append(anchor);
-              fragment.append(tab);
-            });
-            gmtt.customTabBar.append(fragment);
-
-            window.addEventListener('hashchange', gmtt.init);
-            window.addEventListener('resize', () => {
-              clearTimeout(gmtt.resizeTimeout);
-              gmtt.resizeTimeout = setTimeout(gmtt.updateHeight, 250);
-            });
-          }
-          if (!gmtt.customTabsContainer) {
-            gmtt.customTabsContainer = document.createElement('div');
-            gmtt.customTabsContainer.id = 'customTabsContainer';
-            gmtt.customTabsContainer.append(gmtt.customTabBar);
-
-            const collapseBtn = document.createElement('div');
-            collapseBtn.className = 'a5B Yn collapseBtn';
-            const collapseBtnBg = document.createElement('span');
-            collapseBtnBg.className = 'a5D collapseBtnBg collapseBtnBgC';
-            collapseBtn.addEventListener('click', () => {
-              if(gmtt.collapsed) {
-                collapseBtnBg.classList.remove('collapseBtnBgC');
-                collapseBtnBg.classList.add('collapseBtnBgE');
-                gmtt.customTabBar.classList.remove('customTabsC');
-                gmtt.customTabBar.classList.add('customTabsE');
-              } else {
-                collapseBtnBg.classList.remove('collapseBtnBgE');
-                collapseBtnBg.classList.add('collapseBtnBgC');
-                gmtt.customTabBar.classList.remove('customTabsE');
-                gmtt.customTabBar.classList.add('customTabsC');
-              }
-              gmtt.collapsed = !gmtt.collapsed;
-              gmtt.updateHeight();
-            });
-            collapseBtn.append(collapseBtnBg);
-            gmtt.customTabsContainer.append(collapseBtn);
-          }
-          gmtt.customTabsContainer.classList.add('customTabsContainer');
-          if (!gmtt.customTabsContainer.parentNode || gmtt.customTabsContainer.parentNode != tabParent) {
-            tabParent.prepend(gmtt.customTabsContainer);
+            gmtt.selected = null;
           }
         }
-        gmtt.updateHeight();
-      } else if (gmtt.totalWait < gmtt.maxWait) {
-        // console.warn('*************** Tab Parent not found yet');
-        window.setTimeout(gmtt.init, gmtt.intWait);
-      } else {
-        // console.error('*************** Tab Parent was never found and the future refused to change :-(');
+
+        let tabParent = false;
+        if (hash[0] == 'inbox') {
+          tabParent = document.querySelector('div.bGI.nH.oy8Mbf.aE3.S4 > div.aKh');
+          if (tabParent) {
+            tabParent.style.height = 'fit-content';
+            tabParent.querySelector('.aKk').style.height = 'auto';
+          }
+        } else {
+          tabParent = document.querySelector('.AO');
+        }
+        // console.log('*************** Current Tab Parent: ', tabParent);
+        if (tabParent && document.querySelector('.vX.UC').offsetParent === null) {
+          // console.log('*************** Tab Parent found');
+          gmtt.totalWait = 0;
+          if (!display) {
+            if (gmtt.customTabsContainer && !gmtt.customTabsContainer.classList.contains('displayNone')) {
+              gmtt.customTabsContainer.classList.add('displayNone');
+            }
+          } else {
+            if (gmtt.customTabsContainer) {
+              if (gmtt.customTabsContainer.classList.contains('displayNone')) {
+                gmtt.customTabsContainer.classList.remove('displayNone');
+              }
+              gmtt.customTabsContainer.querySelectorAll('.customTab').forEach(customTab => {
+                if (gmtt.selected && customTab.id == `ct-${gmtt.selected}`) {
+                  if (!customTab.classList.contains('ctSelected')) {
+                    customTab.classList.add('ctSelected');
+                  }
+                } else {
+                  if (customTab.classList.contains('ctSelected')) {
+                    customTab.classList.remove('ctSelected');
+                  }
+                }
+              });
+            } else {
+              gmtt.customTabBar = document.createElement('div');
+              gmtt.customTabBar.id = 'customTabBar';
+              gmtt.customTabBar.className = 'aKh aKx customTabs customTabsC';
+              const fragment = document.createDocumentFragment();
+              gmtt.tabsConfig.forEach(tabCfg => {
+                const tab = document.createElement('div');
+                tab.id = `ct-${tabCfg.id}`;
+                tab.className = `aKz customTab ${tabCfg.id == gmtt.selected ? ' ctSelected' : ''}`;
+
+                let url = gmtt.baseUrl;
+                if (/^[\w/]+$/.test(tabCfg.query)) {
+                  url += `#${tabCfg.query}`;
+                } else if (/(&|=)/.test(tabCfg.query)) {
+                  url += `#advanced-search/${encodeURIComponent(tabCfg.query)}${isrefinementParam}`;
+                } else {
+                  url += `#search/${encodeURIComponent(tabCfg.query)}`;
+                }
+                const anchor = document.createElement('a');
+                anchor.id = `ct-a-${tabCfg.id}`;
+                anchor.href = url;
+                anchor.setAttribute('target', '_top');
+                anchor.setAttribute('aria-label', tabCfg.label);
+                anchor.setAttribute('tabindex', -1);
+                anchor.setAttribute('draggable', false);
+
+                const aIcon = document.createElement('div');
+                aIcon.className = 'customTabIcon';
+                aIcon.style.backgroundImage = `url('${tabCfg.icon}')`;
+                anchor.append(aIcon);
+
+                const aLabel = document.createElement('div');
+                aLabel.innerText = tabCfg.label;
+                anchor.append(aLabel);
+
+                tab.append(anchor);
+                fragment.append(tab);
+              });
+              gmtt.customTabBar.append(fragment);
+            }
+            if (!gmtt.customTabsContainer) {
+              gmtt.customTabsContainer = document.createElement('div');
+              gmtt.customTabsContainer.id = 'customTabsContainer';
+              gmtt.customTabsContainer.append(gmtt.customTabBar);
+
+              const collapseBtn = document.createElement('div');
+              collapseBtn.className = 'a5B Yn collapseBtn';
+              const collapseBtnBg = document.createElement('span');
+              collapseBtnBg.className = 'a5D collapseBtnBg collapseBtnBgC';
+              collapseBtn.addEventListener('click', () => {
+                if(gmtt.collapsed) {
+                  collapseBtnBg.classList.remove('collapseBtnBgC');
+                  collapseBtnBg.classList.add('collapseBtnBgE');
+                  gmtt.customTabBar.classList.remove('customTabsC');
+                  gmtt.customTabBar.classList.add('customTabsE');
+                } else {
+                  collapseBtnBg.classList.remove('collapseBtnBgE');
+                  collapseBtnBg.classList.add('collapseBtnBgC');
+                  gmtt.customTabBar.classList.remove('customTabsE');
+                  gmtt.customTabBar.classList.add('customTabsC');
+                }
+                gmtt.collapsed = !gmtt.collapsed;
+                gmtt.updateHeight();
+              });
+              collapseBtn.append(collapseBtnBg);
+              gmtt.customTabsContainer.append(collapseBtn);
+            }
+            gmtt.customTabsContainer.classList.add('customTabsContainer');
+            if (!gmtt.customTabsContainer.parentNode || gmtt.customTabsContainer.parentNode != tabParent) {
+              tabParent.prepend(gmtt.customTabsContainer);
+            }
+          }
+          gmtt.updateHeight();
+        } else if (gmtt.totalWait < gmtt.maxWait) {
+            // console.warn('*************** Tab Parent not found yet');
+          window.setTimeout(gmtt.init, gmtt.intWait);
+        } else {
+          console.error('*************** Tab Parent was never found and the future refused to change :-(');
+        }
+        return this;
       }
-      return this;
     }
   };
+  window.addEventListener('hashchange', gmtt.init);
+  window.addEventListener('resize', () => {
+    clearTimeout(gmtt.resizeTimeout);
+    gmtt.resizeTimeout = setTimeout(gmtt.updateHeight, 250);
+  });
   gmtt.init();
 })();
